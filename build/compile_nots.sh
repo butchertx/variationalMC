3d65851e3d8128c5fa0a32371135829dca15b45f
Matthew <butcher@rice.edu>
2022-02-06 20:17:59 -0600

    Update NOTS compile script

--- build/compile_nots.sh
+++ build/compile_nots.sh
@@ -10,6 +10,7 @@ icpc -c -O2 -std=c++20 ../src/MeanFieldAnsatz.cpp
 icpc -c -O2 -std=c++20 ../src/Wavefunction.cpp
 icpc -c -O2 -std=c++20 ../src/ProjectedState.cpp
 icpc -c -O2 -std=c++20 ../src/vmc_numerical.cpp
+icpc -c -O2 -std=c++20 ../src/model_and_calculation_helper.cpp
 icpc -c -O2 -std=c++20 ../src/VariationalMonteCarlo.cpp
 icpc -c -O2 -std=c++20 ../src/main.cpp
-icpc -O2 -L$LD_LIBRARY_PATH -I$CPATH -lgsl -lgslcblas -lmkl -liomp5 -lpthread vmctype.o vmc_io.o Lattice.o MeanFieldAnsatz.o Wavefunction.o ProjectedState.o vmc_numerical.o VariationalMonteCarlo.o main.o -o ../bin/vmc++.exe
+icpc -O2 -L$LD_LIBRARY_PATH -I$CPATH -lgsl -lgslcblas -lmkl -liomp5 -lpthread vmctype.o vmc_io.o Lattice.o MeanFieldAnsatz.o Wavefunction.o ProjectedState.o vmc_numerical.o model_and_calculation_helper.o VariationalMonteCarlo.o main.o -o ../bin/vmc++.exe
