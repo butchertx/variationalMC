45cdb27f2b40297a067167541fec66a7512d52f1
matthew <mbutcher@utexas.edu>
2023-11-29 17:09:28 -0600

    MWE of CMake program for debugging

---
+++ src/latticex.cpp
@@ -0,0 +1,13 @@
+/**
+ * Source for the latticex executable.
+ * This can be used to produce the lattice output for a lattice.json file
+*/
+
+#include "latticex.h"
+#include "vmctype.h"
+#include "vmc_io.h"
+
+int main(int argc, char* argv[]){
+    int i = 1;
+    std::cout << "Hello world!\n";
+}
